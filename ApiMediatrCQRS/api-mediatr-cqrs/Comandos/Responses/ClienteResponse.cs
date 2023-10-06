8ededc9f088895693e3d78ec1fc1d556d4c44f44
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2023-10-06 16:38:48 -0300

    Create ClienteResponse.cs

---
+++ ApiMediatrCQRS/api-mediatr-cqrs/Comandos/Responses/ClienteResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace api_mediatr_cqrs.Comandos.Responses
+{
+    public class ClienteResponse
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+        public DateTime Data { get; set; }
+    }
+}

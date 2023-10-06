9c583ca7b80e084ccfb8c0f02394ba525e4adc6a
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2023-10-06 16:38:55 -0300

    Create Cliente.cs

---
+++ ApiMediatrCQRS/api-mediatr-cqrs/Dominio/Entidades/Cliente.cs
@@ -0,0 +1,15 @@
+﻿namespace api_mediatr_cqrs.Dominio.Entidades
+{
+    public class Cliente
+    {
+        public Cliente(string nome, string telefone)
+        {
+            Nome = nome;
+            Telefone = telefone;
+        }
+
+        public Guid IdCliente { get; set; }
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+    }
+}

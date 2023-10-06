b12276121317fb92f23eaee228fea71516ef7a0c
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2023-10-06 16:38:42 -0300

    Create ClienteRequest.cs

---
+++ ApiMediatrCQRS/api-mediatr-cqrs/Comandos/Requests/ClienteRequest.cs
@@ -0,0 +1,11 @@
+﻿using api_mediatr_cqrs.Comandos.Responses;
+using MediatR;
+
+namespace api_mediatr_cqrs.Comandos.Requests
+{
+    public class ClienteRequest : IRequest<ClienteResponse>
+    {
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+    }
+}

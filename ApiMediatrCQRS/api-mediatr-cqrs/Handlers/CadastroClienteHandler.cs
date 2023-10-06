bc885323a92b1b003692e11aa7e5ceccbe33635c
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2023-10-06 16:39:00 -0300

    Create CadastroClienteHandler.cs

---
+++ ApiMediatrCQRS/api-mediatr-cqrs/Handlers/CadastroClienteHandler.cs
@@ -0,0 +1,24 @@
+﻿using api_mediatr_cqrs.Comandos.Requests;
+using api_mediatr_cqrs.Comandos.Responses;
+using MediatR;
+
+namespace api_mediatr_cqrs.Handlers
+{
+    public class CadastroClienteHandler : IRequestHandler<ClienteRequest, ClienteResponse>
+    {
+        public Task<ClienteResponse> Handle(ClienteRequest request, CancellationToken cancellationToken)
+        {
+            // Aqui entra a regra de cadastro de cliente
+            // Abaixo simulação do retorno
+            ClienteResponse resultado = new()
+            {
+                Id = Guid.NewGuid(),
+                Nome = "Bruce Wayne",
+                Telefone = "(11)55661122",
+                Data = DateTime.Now
+            };
+
+            return Task.FromResult(resultado);
+        }
+    }
+}

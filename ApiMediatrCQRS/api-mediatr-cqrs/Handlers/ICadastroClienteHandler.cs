32db91776b543c72834ab982d91f891099b21ba5
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2023-10-06 16:39:06 -0300

    Create ICadastroClienteHandler.cs

---
+++ ApiMediatrCQRS/api-mediatr-cqrs/Handlers/ICadastroClienteHandler.cs
@@ -0,0 +1,10 @@
+﻿using api_mediatr_cqrs.Comandos.Requests;
+using api_mediatr_cqrs.Comandos.Responses;
+
+namespace api_mediatr_cqrs.Handlers
+{
+    public interface ICadastroClienteHandler
+    {
+        ClienteResponse Handle(ClienteRequest request);
+    }
+}
